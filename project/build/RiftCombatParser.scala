09c9dd743909cf7103575c0773566766aca9bdd2
Gordon Tyler <gordon@doxxx.net>
2011-03-18 17:21:11 -0400

    Set GUIMain as project mainclass.

--- project/build/RiftCombatParser.scala
+++ project/build/RiftCombatParser.scala
@@ -2,4 +2,5 @@ import sbt._
 
 class RiftCombatParser(info: ProjectInfo) extends DefaultProject(info) {
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
+  override def mainClass = Some("net.doxxx.riftcombatparser.GUIMain")
 }
