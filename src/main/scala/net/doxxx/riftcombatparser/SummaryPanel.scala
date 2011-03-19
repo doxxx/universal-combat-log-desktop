db6826fb0f299f6b3b8de9d026a6a5e93fe04a20
Gordon Tyler <gordon@doxxx.net>
2011-03-18 21:18:55 -0400

    Fixed sorting in the table.

--- src/main/scala/net/doxxx/riftcombatparser/SummaryPanel.scala
+++ src/main/scala/net/doxxx/riftcombatparser/SummaryPanel.scala
@@ -1,12 +1,19 @@
 package net.doxxx.riftcombatparser
 
 import swing._
-import javax.swing.table.AbstractTableModel
+import collection.JavaConversions._
 import collection.immutable.TreeSet
+import javax.swing.table.{TableRowSorter, AbstractTableModel}
+import java.util.Comparator
+import javax.swing.{SortOrder, RowSorter}
 
 class SummaryPanel(events: List[Event]) extends ScrollPane {
   contents = new Table {
-     model = new DamageSummaryModel(events)
+    model = new DamageSummaryModel(events)
+    val rowSorter = new TableRowSorter(model)
+    rowSorter.setComparator(1, new IntComparator)
+    rowSorter.setSortKeys(List(new RowSorter.SortKey(1, SortOrder.DESCENDING)))
+    peer.setRowSorter(rowSorter)
   }
 }
 
@@ -30,4 +37,8 @@ class DamageSummaryModel(events: List[Event]) extends AbstractTableModel {
   def getColumnCount = 2
 
   def getRowCount = data.size
+}
+
+class IntComparator extends Comparator[Int] {
+  def compare(o1: Int, o2: Int) = o1 - o2
 }
