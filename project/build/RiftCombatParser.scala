cba53faf2e0747a82baf9c541461e30cc6b00e35
Gordon Tyler <gordon@doxxx.net>
2011-03-18 21:18:25 -0400

    Switched to Scala 2.9.0-SNAPSHOT and a locally built snapshot of the Scala Swing incubator project.

--- project/build/RiftCombatParser.scala
+++ project/build/RiftCombatParser.scala
@@ -1,6 +1,7 @@
 import sbt._
 
 class RiftCombatParser(info: ProjectInfo) extends DefaultProject(info) {
-  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
+//  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
+  val scalaSwing = "EPFL" %% "scala-swing" % "1.0"
   override def mainClass = Some("net.doxxx.riftcombatparser.GUIMain")
 }
