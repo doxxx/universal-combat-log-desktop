a3a8de6251370d9dca2b7d3651ca55da1a481bfc
Gordon Tyler <gordon@doxxx.net>
2011-03-20 19:12:59 -0400

    Require sources for Swing library.

--- project/build/RiftCombatParser.scala
+++ project/build/RiftCombatParser.scala
@@ -2,6 +2,6 @@ import sbt._
 
 class RiftCombatParser(info: ProjectInfo) extends DefaultProject(info) {
 //  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
-  val scalaSwing = "EPFL" %% "scala-swing" % "1.0"
+  val scalaSwing = "EPFL" %% "scala-swing" % "1.0" withSources()
   override def mainClass = Some("net.doxxx.riftcombatparser.GUIMain")
 }
