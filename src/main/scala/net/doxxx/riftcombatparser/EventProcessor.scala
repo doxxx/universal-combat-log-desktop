79cd660eed819f5d51a847bca5668eea901afa84
Gordon Tyler <gordon@doxxx.net>
2011-03-21 19:25:47 -0400

    Refactored summary processing.

--- src/main/scala/net/doxxx/riftcombatparser/EventProcessor.scala
+++ src/main/scala/net/doxxx/riftcombatparser/EventProcessor.scala
@@ -4,45 +4,28 @@ import collection.mutable.HashMap
 import EventType._
 
 object EventProcessor {
-  def isDamageEvent(e: ActorEvent): Boolean = {
-    e.eventType == EventType.DirectDamage || e.eventType == EventType.DamageOverTime || e.eventType == EventType.DamageOverTime
+  def isDamageEvent(eventType: EventType.Value): Boolean = {
+    eventType == DirectDamage || eventType == DamageOverTime || eventType == DamageOverTime
   }
 
-  private def addDamage(results: HashMap[String, Summary], actor: String, target: String, amount: Int) {
-    if (!results.contains(actor)) {
-      results(actor) = new Summary(0, 0, 0, 0)
-    }
-    results(actor).damageOut += amount
-
-    if (!results.contains(target)) {
-      results(target) = new Summary(0, 0, 0, 0)
-    }
-    results(target).damageIn += amount
-  }
-
-  private def addHealing(results: HashMap[String, Summary], actor: String, target: String, amount: Int) {
-    if (!results.contains(actor)) {
-      results(actor) = new Summary(0, 0, 0, 0)
-    }
-    results(actor).healingOut += amount
-
-    if (!results.contains(target)) {
-      results(target) = new Summary(0, 0, 0, 0)
-    }
-    results(target).healingIn += amount
+  def isHealEvent(eventType: EventType.Value): Boolean = {
+    eventType == Heal || eventType == CritHeal
   }
 
   def summary(events: List[Event]) = {
-    val results = new HashMap[String, Summary]
+    val results = new HashMap[String, Summary] {
+      override def default(key: String) = Summary()
+    }
     for (e <- events) e match {
-      case ae: ActorEvent => ae.eventType match {
-        case DamageOverTime => addDamage(results, ae.actor, ae.target, ae.amount)
-        case DirectDamage => addDamage(results, ae.actor, ae.target, ae.amount)
-        case EnvDamage => addDamage(results, ae.actor, ae.target, ae.amount)
-        case CritDamage => addDamage(results, ae.actor, ae.target, ae.amount)
-        case Heal => addHealing(results, ae.actor, ae.target, ae.amount)
-        case CritHeal => addHealing(results, ae.actor, ae.target, ae.amount)
-        case _ =>
+      case ae: ActorEvent if (isDamageEvent(ae.eventType)) => {
+        results(ae.actor) = results(ae.actor).addDamageOut(ae.amount)
+        results(ae.target) = results(ae.target).addDamageIn(ae.amount)
+
+      }
+      case ae: ActorEvent if (isHealEvent(ae.eventType)) => {
+        results(ae.actor) = results(ae.actor).addHealingOut(ae.amount)
+        results(ae.target) = results(ae.target).addHealingIn(ae.amount)
+
       }
       case _ =>
     }
@@ -51,7 +34,9 @@ object EventProcessor {
 
 }
 
-class Summary(var damageIn: Int,
-              var damageOut: Int,
-              var healingIn: Int,
-              var healingOut: Int)
+case class Summary(damageIn: Int = 0, damageOut: Int = 0, healingIn: Int = 0, healingOut: Int = 0) {
+  def addDamageIn(amount: Int) = copy(damageIn = damageIn + amount)
+  def addDamageOut(amount: Int) = copy(damageOut = damageOut + amount)
+  def addHealingIn(amount: Int) = copy(healingIn = healingIn + amount)
+  def addHealingOut(amount: Int) = copy(healingOut = healingOut + amount)
+}
