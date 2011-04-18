b4a34d75f0d7df623492a7b6948f2b13b491199b
Gordon Tyler <gordon@doxxx.net>
2011-04-18 00:12:50 -0400

    Added StatusBar component.

---
+++ src/main/scala/net/doxxx/riftcombatparser/StatusBar.scala
@@ -0,0 +1,16 @@
+package net.doxxx.riftcombatparser
+
+import swing._
+
+class StatusBar extends BoxPanel(Orientation.Horizontal) {
+  val message = new Label
+  val progress = new ProgressBar
+
+  contents += message
+  contents += Swing.HStrut(5)
+  contents += progress
+
+  def update(message: String) {
+    this.message.text = message
+  }
+}
