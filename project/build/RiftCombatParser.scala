3d4dfde4a1df091e053b382244cd29b0461634fc
Gordon Tyler <gordon@doxxx.net>
2011-04-23 01:02:19 -0400

    Added ScalaTest library to project.

--- project/build/RiftCombatParser.scala
+++ project/build/RiftCombatParser.scala
@@ -1,7 +1,11 @@
 import sbt._
 
 class RiftCombatParser(info: ProjectInfo) extends DefaultProject(info) {
+  val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots"
+  
 //  val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1"
   val scalaSwing = "EPFL" %% "scala-swing" % "1.0" withSources() withJavadoc()
+  val scalaTest = "org.scalatest" % "scalatest" % "1.4-SNAPSHOT" withSources() withJavadoc()
   override def mainClass = Some("net.doxxx.riftcombatparser.GUIMain")
+  //override def compileOptions = super.compileOptions ++ List(Unchecked)
 }
