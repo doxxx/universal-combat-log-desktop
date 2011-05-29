1d527e068c37cf3484ebd1f5be5baa6a90ab8942
Gordon Tyler <gordon@doxxx.net>
2011-05-28 20:50:23 -0400

    Added sorting to spell breakdown table.

--- src/main/scala/net/doxxx/riftcombatparser/SpellBreakdownPanel.scala
+++ src/main/scala/net/doxxx/riftcombatparser/SpellBreakdownPanel.scala
@@ -1,11 +1,22 @@
 package net.doxxx.riftcombatparser
 
+import collection.JavaConversions._
 import swing.{Table, ScrollPane}
+import javax.swing.table.TableRowSorter
+import javax.swing.{SortOrder, RowSorter}
 
 class SpellBreakdownPanel extends ScrollPane {
   val spellBreakdownModel = new SpellBreakdownModel
   val table = new Table {
     model = spellBreakdownModel
+    val rowSorter = new TableRowSorter(model)
+    rowSorter.setComparator(1, IntComparator)
+    rowSorter.setComparator(2, IntComparator)
+    rowSorter.setComparator(3, IntComparator)
+    rowSorter.setComparator(4, IntComparator)
+    rowSorter.setComparator(5, IntComparator)
+    rowSorter.setSortKeys(List(new RowSorter.SortKey(1, SortOrder.DESCENDING)))
+    peer.setRowSorter(rowSorter)
   }
 
   contents = table
