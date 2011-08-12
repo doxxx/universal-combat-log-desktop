71f2c7330d928a956283b29a9064315901babb8e
Gordon Tyler <gordon@doxxx.net>
2011-08-12 18:02:55 -0400

    Added PopupMenu.

---
+++ src/main/scala/net/doxxx/riftcombatparser/PopupMenu.scala
@@ -0,0 +1,12 @@
+package net.doxxx.riftcombatparser
+
+import javax.swing.JPopupMenu
+import swing.{SequentialContainer, Component}
+
+class PopupMenu(title0: String) extends Component with SequentialContainer.Wrapper { self: PopupMenu =>
+  override lazy val peer: JPopupMenu = new JPopupMenu(title0)
+
+  def show(invoker: Component, x: Int, y: Int) {
+    peer.show(invoker.peer, x, y)
+  }
+}
